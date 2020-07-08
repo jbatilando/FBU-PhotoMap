15d909970e4e9ef7461816807ecf848aa2281d84
Miguel Batilando <miguel.batilando@gmail.com>
2020-07-07 22:33:43 -0700

    User can see pin with annotation when selecting location

--- PhotoMap/View Controllers/PhotoMapViewController.m
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -10,9 +10,9 @@
 #import <MapKit/MapKit.h>
 #import "LocationsViewController.h"
 
-@interface PhotoMapViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, LocationsViewControllerDelegate>
+@interface PhotoMapViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, LocationsViewControllerDelegate, MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *map;
-@property (weak, nonatomic) IBOutlet UIImage *selectedImage;
+@property (weak, nonatomic) UIImage *selectedImage;
 @end
 
 @implementation PhotoMapViewController
@@ -20,8 +20,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.map.delegate = self;
     MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
     [self.map setRegion:sfRegion animated:false];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,12 +34,27 @@
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
     // Assign the image to a property so that it can be used later
     UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
-    self.selectedImage = originalImage;
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    self.selectedImage = editedImage;
     // Dismiss the modal camera view controller you previously presented.
     [self dismissViewControllerAnimated:YES completion:^{
         // Launch the LocationsViewController in the completion block of dismissing the camera modal using the segue identifier tagSegue.
         [self performSegueWithIdentifier:@"tagSegue" sender:nil];
     }];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
 }
 
 - (IBAction)didTapOnButton:(id)sender {
@@ -65,6 +82,30 @@
 - (void)locationsViewController:(id)controller didPickLocationWithLatitude:(NSNumber *)latitude longitude:(NSNumber *)longitude {
     // Pop to VC
     [self.navigationController popToViewController:self animated:YES];
+    NSLog(@"%@ %@", latitude, longitude);
+    // Milestone 5
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude.floatValue, longitude.floatValue);
+    // Add annotation
+    MKPointAnnotation *annotation = [MKPointAnnotation new];
+    annotation.coordinate = coordinate;
+    annotation.title = @"Picture!";
+    [self.map addAnnotation:annotation];
+    [self.map viewForAnnotation:annotation];
+
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *annotationView = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"Pin"];
+    if (annotationView == nil) {
+        annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+        annotationView.canShowCallout = true;
+        annotationView.leftCalloutAccessoryView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 50.0, 50.0)];
+    }
+
+    UIImageView *imageView = (UIImageView*)annotationView.leftCalloutAccessoryView;
+    imageView.image = [UIImage imageNamed:@"camera-icon"];
+
+    return annotationView;
 }
 
 @end
